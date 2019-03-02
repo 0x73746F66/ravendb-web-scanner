d3b8986d4be2e01126b15782e8032ca4fec9018d
Chris Langton <chris.langton@cmdsolutions.com.au>
2019-03-02 23:22:40 +1100

    fixed ravendb settings

--- ravendb-docker.sh
+++ ravendb-docker.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
+# /mnt/ravendb/config/license.json
 mkdir -p /mnt/ravendb/mnt/ravendb/data /mnt/ravendb/config /mnt/ravendb/backup
 
 # /mnt/ravendb/config/settings.json
 # {
 #     "ServerUrl": "http://127.0.0.1:8080",
-#     "Setup.Mode": "Initial",
-#     "Server.LocalRootPath": "/opt/RavenDB/Backup"
+#     "PublicServerUrl": "http://ravendb.langton.cloud:8080",
+#     "Setup.Mode": "None",
+#     "Server.LocalRootPath": "/opt/RavenDB/Backup",
+#     "Security.Certificate.Path": "/opt/RavenDB/",
+#     "Security.Certificate.Password": ""
 # }
 
-# /mnt/ravendb/config/license.json
-
-
-docker run -d --rm --name ravendb \
-    -p 8080:8080 \
-    -p 38888:38888 \
-    -e RAVEN_ARGS='--Setup.Mode=None' \
+docker run -d --name ravendb \
+    --network="host" \
+    --add-host ravendb.langton.cloud:10.1.1.127 \
+    -e RAVEN_ServerUrl_Tcp=38888 \
+    -e RAVEN_Setup_Mode='None' \
     -e RAVEN_Security_UnsecuredAccessAllowed='PrivateNetwork' \
+    --restart=unless-stopped \
     --mount type=bind,src=/mnt/ravendb/data,dst=/opt/RavenDB/Server/RavenData \
-    --mount type=bind,src=/mnt/ravendb/config,dst=/opt/RavenDB/config \
+    --mount type=bind,src=/mnt/ravendb/config/settings.json,dst=/opt/RavenDB/settings.json,readonly \
     --mount type=bind,src=/mnt/ravendb/backup,dst=/opt/RavenDB/Backup \
     ravendb/ravendb:4.1.4-ubuntu.18.04-x64
